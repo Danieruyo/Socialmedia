5fcdb2673455e9c08179758f07d3e2c327b21d04
Daniel de Abreu <danielabreuintz@gmail.com>
2024-04-12 17:10:57 -0300

    primeiro commit

---
+++ special/social/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Post, Comment, MessageModel
+
+class PostForm(forms.ModelForm):
+    body = forms.CharField(
+        label='',    
+        widget=forms.Textarea(attrs={ 
+            'row': '3',
+            'placeholder': 'Say Something...'
+            }))
+    
+    image = forms.ImageField(
+        required=False     
+    )
+    class Meta:
+        model = Post
+        fields = ['body', 'image']
+   
+class CommentForm(forms.ModelForm):
+    comment = forms.CharField(
+        label='',    
+        widget=forms.Textarea(attrs={ 
+            'row': '3',
+            'placeholder': 'Say Something...' }))
+    class Meta:
+        model = Comment
+        fields = ['comment']
+class ThreadForm(forms.Form):
+    username = forms.CharField(label='', max_length=100)
+    
+class MessageForm(forms.ModelForm):
+    body = forms.CharField(label='', max_length=1000)
+
+    image = forms.ImageField(required=False)
+    class Meta:
+        model = MessageModel
+        fields = ['body', 'image']
---
+++ special/social/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollowers, AddLike, AddDislike, UserSearch, ListFollowers, AddCommentLike, AddCommentDislike, CommentReplyView, PostNotification, FollowNotification, ThreadNotification, RemoveNotification, CreateThread, ListThreads, ThreadView, CreateMessage
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name='comment-like'),
+    path('post/<int:post_pk>/comment/<int:pk>dislike', AddCommentDislike.as_view(), name='comment-dislike'),
+    path('post/<int:post_pk>/comment/<int:pk>/reply', CommentReplyView.as_view(), name='comment-reply'),
+    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('post/<int:pk>/dislike', AddDislike.as_view(), name='dislike'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
+    path('profile/<int:pk>/followers/', ListFollowers.as_view(), name='list-followers'),
+    path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
+    path('profile/<int:pk>/followers/remove', RemoveFollowers.as_view(), name='remove-follower'),
+    path('search/', UserSearch.as_view(), name='profile-search'),
+    path('notification/<int:notification_pk>/post/<int:post_pk>', PostNotification.as_view(), name='post-notification'),
+    path('notification/<int:notification_pk>/profile/<int:profile_pk>', FollowNotification.as_view(), name='follow-notification'),
+    path('notification/<int:notification_pk>/thread/<int:object_pk>', ThreadNotification.as_view(), name='thread-notification'),
+    path('notification/delete/<int:notification_pk>', RemoveNotification.as_view(), name='notification-delete'),
+    path('inbox/', ListThreads.as_view(), name='inbox'),
+    path('inbox/create-thread/', CreateThread.as_view(), name='create-thread'),
+    path('inbox/<int:pk>/', ThreadView.as_view(), name='thread'),
+    path('inbox/<int:pk>/create-message/', CreateMessage.as_view(), name='create-message'),
+  
+]
